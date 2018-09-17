f25375a4de46ee0c5f9dc73ade514cd1cc186195
SuengBumKim <sb921204@naver.com>
2018-09-17 17:19:08 +0900

    Drills_05 Calculate inclination

--- Drills5/drills_05.py
+++ Drills5/drills_05.py
@@ -1,4 +1,5 @@
 from pico2d import *
+from math
 
 open_canvas()
 grass = load_image('grass.png')
@@ -8,13 +9,29 @@ character = load_image('animation_sheet.png')
 x = 0
 frame = 0
 
-position = {203 : 535 , 132 : 243, 535 : 470,
+position = {132 : 243, 535 : 470,
             477 : 203, 715 : 136, 316 : 225,
-            510 : 92, 692 : 518, 682 : 336, 712 : 349}
+            510 : 92, 692 : 518, 682 : 336, 712 : 349,
+            203 : 535}
+
 
 def run_from_pos_to_pos():
-    for pos in position:
-        pos.keys()
+    x, y = 203, 535
+
+    for posX, posY in position.items():
+        distX = math.fabs(posX - x)
+        distY = math.fabs(posY - y)
+        inclination = distY / distX
+
+    while x != :
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        delay(0.05)
+
 
 while True:
     run_from_pos_to_pos()
