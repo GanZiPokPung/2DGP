e700a53a411ed641f86f17e59b5c148b51be3a27
SuengBumKim <sb921204@naver.com>
2018-09-17 17:57:46 +0900

    drills_05 definition error fixed

--- Drills5/drills_05.py
+++ Drills5/drills_05.py
@@ -1,13 +1,10 @@
 from pico2d import *
-from math
+import math
 
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-
-x = 0
-frame = 0
 
 position = {132 : 243, 535 : 470,
             477 : 203, 715 : 136, 316 : 225,
@@ -16,32 +13,30 @@ position = {132 : 243, 535 : 470,
 
 
 def run_from_pos_to_pos():
+    frame = 0
     x, y = 203, 535
 
     for posX, posY in position.items():
-        checkX = 0
-        distX = math.fabs(posX - x)
-        distY = math.fabs(posY - y)
-        inclination = distY / distX
+        count = 0
+        gotoX = (posX - x) / 100
+        gotoY = (posY - y) / 100
 
         if(posX - x) > 0:
             checkRight = True
         else:
             checkRight = False
 
-        while checkX < distX :
+        while count <= 100 :
             clear_canvas()
             grass.draw(400, 30)
             if checkRight == True:
                 character.clip_draw(frame * 100, 100, 100, 100, x, y)
-                x += 1
-                y = y + (inclination * x)
             else:
                 character.clip_draw(frame * 100, 0, 100, 100, x, y)
-                x -= 1
-                y = y + (inclination * x)
-            checkX += 1
             update_canvas()
+            count += 1
+            x += gotoX
+            y += gotoY
             frame = (frame + 1) % 8
             delay(0.05)
 
