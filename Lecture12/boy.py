b6c3653903c6f98568cd87d38803c4570bd7f53b
GanZiPokPung <sb9212@kpu.ac.kr>
2018-10-29 23:46:21 +0900

    ghost add

--- Lecture12/boy.py
+++ Lecture12/boy.py
@@ -3,6 +3,8 @@ from pico2d import *
 from ball import Ball
 
 import game_world
+import random
+import math
 
 # Boy Run Speed
 # fill expressions correctly
@@ -18,6 +20,8 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
+# Ghost Boy
+ANGLE_PER_SEC = 360.0 * 2
 
 
 # Boy Event
@@ -108,14 +112,15 @@ class SleepState:
     @staticmethod
     def enter(boy, event):
         boy.frame = 0
-
+        boy.ghost = GhostBoy(boy.x, boy.y)
     @staticmethod
     def exit(boy, event):
-        pass
+        del boy.ghost
 
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        boy.ghost.update()
 
     @staticmethod
     def draw(boy):
@@ -123,7 +128,7 @@ class SleepState:
             boy.image.clip_composite_draw(int(boy.frame) * 100, 300, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
         else:
             boy.image.clip_composite_draw(int(boy.frame) * 100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
-
+        boy.ghost.draw()
 
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SLEEP_TIMER: SleepState, SPACE: IdleState},
@@ -145,6 +150,7 @@ class Boy:
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
         self.alertTimer = 0
+        self.ghost = 0
 
     def fire_ball(self):
         ball = Ball(self.x, self.y, self.dir*3)
@@ -171,3 +177,36 @@ class Boy:
             key_event = key_event_table[(event.type, event.key)]
             self.add_event(key_event)
 
+class GhostBoy:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.originY = y
+        self.image = load_image('animation_sheet.png')
+        self.dir = 1
+        self.velocity = 0
+        self.frame = 0
+
+        self.angle = 90
+        self.upCheck = True
+        self.radius = PIXEL_PER_METER * 3
+        self.circleTimer = ANGLE_PER_SEC / 60.0
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        self.image.opacify(random.randint(0, 100) / 100)
+
+        if self.upCheck == True:
+            if self.radius + self.originY > self.y :
+                self.y += self.radius / 60.0
+            else:
+                self.upCheck = False
+        else:
+            self.angle += self.circleTimer
+            self.x += math.cos(math.radians(self.angle)) * self.radius
+            self.y += math.sin(math.radians(self.angle)) * self.radius
+
+    def draw(self):
+        if self.dir == 1:
+            self.image.clip_draw(int(self.frame) * 100, 300, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(int(self.frame) * 100, 200, 100, 100, self.x, self.y)
